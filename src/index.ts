7a28f9be1910592a0f631afcb54f3924f2fd01ff
Ryan Adams <ryan.g.adams@gmail.com>
2024-05-26 20:41:37 +0100

    Adding open in browser code.
    Showing a dialog when there is no link because I can't update or remove the button based on the current note's status.

--- src/index.ts
+++ src/index.ts
@@ -1,35 +1,25 @@
 import joplin from 'api';
+import {ToolbarButtonLocation} from '../api/types';
+
+const pluginName = 'openInBrowser';
+
+async function onStart() {
+	await joplin.commands.register({
+		name: pluginName,
+		label: 'Open in browser',
+		iconName: 'fas fa-external-link-alt ',
+		execute: async () => {
+			const note = await joplin.workspace.selectedNote();
+			if (note.source_url === '') {
+				return await joplin.views.dialogs.showMessageBox('No link')
+			}
+			await joplin.commands.execute('openItem', note.source_url);
+		},
+	});
+
+	await joplin.views.toolbarButtons.create(pluginName, pluginName, ToolbarButtonLocation.EditorToolbar);
+}
 
 joplin.plugins.register({
-	onStart: async function() {
-		// eslint-disable-next-line no-console
-		console.info('Hello world. Test plugin started!');
-
-		async function updateTocView() {
-			// Get the current note from the workspace.
-			const note = await joplin.workspace.selectedNote();
-
-			// Keep in mind that it can be `null` if nothing is currently selected!
-			if (note) {
-				console.info('Note content has changed! New note is:', note);
-			} else {
-				console.info('No note is selected');
-			}
-		}
-
-		// This event will be triggered when the user selects a different note
-		await joplin.workspace.onNoteSelectionChange(() => {
-			updateTocView();
-		});
-
-		// This event will be triggered when the content of the note changes
-		// as you also want to update the TOC in this case.
-		await joplin.workspace.onNoteChange(() => {
-			updateTocView();
-		});
-
-		// Also update the TOC when the plugin starts
-		updateTocView();
-
-	},
+	onStart,
 });
